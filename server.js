da708a7a48617244d1e03b945f2b41c202b3142a
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-08-20 04:58:10 -0400

    change PORT binding for prod deploy

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const path = require('path')
 
 const app = express();
 
-const port = process.env.API_PORT || 5000;
+//process.env.PORT for HEROKU DYNAMIC ASSNMT
+const port = process.env.PORT || 5000;
+//const port = process.env.API_PORT || 5000;
 const appOrigin = "http://localhost:3000"
 //process.env.APP_ORIGIN;
 const audience = "sample.express-api.com"
