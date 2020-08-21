6ae66032698a0f6592235607a2883f5002af8cc8
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-08-21 04:05:27 -0400

    __dirname variable fix

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req,res) => {
-    res.sendFile(path.join(_dirname, 'client/build/index.html' )); //pass relative path
+    res.sendFile(path.join(__dirname, 'client/build/index.html' )); //pass relative path
   })
 }
 
