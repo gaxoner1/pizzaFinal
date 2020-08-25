c4b8c46c3250bc55d805719d5ba3af9ce3b3f56b
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-08-25 16:09:49 -0400

    Order API call debug

--- client/src/views/order.js
+++ client/src/views/order.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Menu from "./Menu";
+import { Loading } from "../components/loading";
 require("dotenv").config();
 
 const SendOrder = () => {
